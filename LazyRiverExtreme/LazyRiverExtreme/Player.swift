29bcfcc477f2a81adb518490abde6a1894bbd837
carnovalejf <44624105+carnovalejf@users.noreply.github.com>
2020-02-13 20:36:12 -0500

    added in basic player move/projectile ability
    this push had the finishing of the player data struct as well as the foundation for the abilities. There is also player movement implemented, and the use of the player projectile.

--- LazyRiverExtreme/LazyRiverExtreme/Player.swift
+++ LazyRiverExtreme/LazyRiverExtreme/Player.swift
@@ -11,11 +11,94 @@ import SpriteKit
 
 class Player {
 
-    var sprite = SKSpriteNode()
-    var health = Float()
-    var speed = Float()
-    var spriteImages : [String]
+    var sprite : SKSpriteNode
+    var health : Float
+    var speed : Float
     var animationNames : [String]
+    var savedData : PlayerData
+    var lastAnimation: String
+    var spriteImageNames : [String]
+    var startingPoint : CGPoint
+    var endingPoint:CGPoint
     
+    init(start: CGPoint, end: CGPoint){
+        let texture = SKTexture(imageNamed: "zombie1")
+        sprite = SKSpriteNode(texture: texture, color:.clear, size:texture.size())
+        health = 100.0
+        speed = 1.0
+        animationNames = [String]()
+        savedData = PlayerData()
+        lastAnimation = String()
+        spriteImageNames = savedData.lastSprites
+        startingPoint = start
+        endingPoint = end
+    }
     
+    func idleAnimation(){
+        var spritetextures = [SKTexture]()
+        for name in spriteImageNames{
+            spritetextures.append(SKTexture(imageNamed: name))
+        }
+        
+        //temporary
+        spritetextures.append(sprite.texture!)
+        
+        let idle = SKAction.animate(with:spritetextures, timePerFrame: 0.5/Double(spritetextures.count))
+        let reverse = idle.reversed()
+        
+        let sequence = SKAction.sequence([idle, reverse])
+        
+        let repeatsequence = SKAction.repeatForever(sequence)
+        
+        sprite.removeAction(forKey: lastAnimation)
+        
+        sprite.run(repeatsequence, withKey:"idle")
+        lastAnimation = "idle"
+    }
+    
+    func move(){
+        /*let move = SKAction.move(by: CGVector(dx: 0, dy: 50), duration: 0.2)
+        //will maybe add a unique moving animation action here
+        let repeatmove = SKAction.repeatForever(move)*/
+        let directionx = endingPoint.x - sprite.position.x
+        let directiony = endingPoint.y - sprite.position.y
+        let mag = sqrt((directionx * directionx) + (directiony * directiony))
+       
+        let move = SKAction.move(to:endingPoint, duration: Double(mag) / 200)
+        
+        sprite.removeAction(forKey: lastAnimation)
+        
+        sprite.run(move, withKey:"move")
+        lastAnimation = "move"
+    }
+    
+    func resetPosition(){
+        let directionx = startingPoint.x - sprite.position.x
+        let directiony = startingPoint.y - sprite.position.y
+        let mag = sqrt((directionx * directionx) + (directiony * directiony))
+       
+        let reset = SKAction.move(to:startingPoint, duration: Double(mag) / 200)
+        
+        sprite.removeAction(forKey: lastAnimation)
+        
+        sprite.run(reset, withKey: "reset")
+        lastAnimation = "reset"
+    }
+    
+    func ability1(scene: SKScene){
+        let projectile = SKSpriteNode(imageNamed: "cat")
+        scene.addChild(projectile)
+        projectile.zPosition = 1
+        projectile.position = CGPoint(x:sprite.position.x, y:sprite.position.y)
+        
+        let forwardvec = CGVector(dx: cos(sprite.zRotation), dy: sin(sprite.zRotation))
+        let projectilelaunch = SKAction.moveBy(x:forwardvec.dx * 500, y:forwardvec.dy * 500, duration: 1)
+        let death = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([projectilelaunch, death])
+        projectile.run(sequence)
+    }
+    
+    func ability2(scene: SKScene){
+        
+    }
 }
--- LazyRiverExtreme/LazyRiverExtreme/PlayerData.swift
+++ LazyRiverExtreme/LazyRiverExtreme/PlayerData.swift
@@ -9,20 +9,30 @@
 import Foundation
 
 class PlayerData{
-    var highScore = Float()
-    var money = Float()
-    var premiumMoney = Float()
-    
+    var highScore : Float
+    var money : Float
+    var premiumMoney : Float
+    var lastSprites : [String]
     //keeping these as ints for now, the idea is to store the item key from the store and reference it
-    var storeUnlocks: Set<Int>
-    var equipment: Set<Int>
+    var storeUnlocks: Set<CShort>
+    var equipment: Set<CShort>
+    var ability1: CShort
+    var ability2: CShort
+    var previousRuns: [Float]
+    var abilities: [CShort: [String]] = [:]
     
     init(){
+        //all of these values will be changed to get initialized from a file/cloud storage
         highScore = 0.0
         money = 0.0
         premiumMoney = 0.0
-        storeUnlocks = Set<Int>()
-        equipment = Set<Int>()
+        storeUnlocks = Set<CShort>()
+        equipment = Set<CShort>()
+        ability1 = 0
+        ability2 = 0
+        lastSprites = [String]()
+        previousRuns = [Float]()
+        //abilities would get loaded here from file
     }
 }
 
