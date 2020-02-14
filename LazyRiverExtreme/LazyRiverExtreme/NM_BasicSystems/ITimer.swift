1d2af5e6a46db241f211cb3c576bbad796fe298d
Gunjudge <45321606+Gunjudge@users.noreply.github.com>
2020-02-13 20:37:02 -0500

    Updated Timer Class
    Finished all the neccessary funtions and member variables for the timer class. Also changed it from an interface/protocol to a class.

--- LazyRiverExtreme/LazyRiverExtreme/NM_BasicSystems/ITimer.swift
+++ LazyRiverExtreme/LazyRiverExtreme/NM_BasicSystems/ITimer.swift
@@ -8,13 +8,41 @@
 
 import Foundation
 
-protocol Timer{
-    let currentTime;
-    //Increment must be the same type as current time
-    let increment;
+class Timer{
+    //Time variables\\
+    //All in seconds\\
+    let timeCreated: Double
+    private var currentTimeElapsed: Double = 0.0 // Current timer elapesed time since start
+    private var previousTimeElasped: Double = 0.0 // The count before the current
+    var currentCount: Double = 0.0
+    private var deltaTime: Double = 0.0
+    private var start: Double = 0.0 //The last time the timer was started
     
-    func StartTimer() -> Bool;
-    func Stop() -> Bool;
+    var isCounting: Bool = false;
+    
+    init() {
+        timeCreated = Date().timeIntervalSinceReferenceDate
+    }
+    
+    
+    func Update(){
+        previousTimeElasped = currentTimeElapsed
+        currentTimeElapsed = Date().timeIntervalSinceReferenceDate - start
+        deltaTime = currentTimeElapsed - previousTimeElasped
+        if(isCounting)
+        {
+            currentCount += deltaTime
+        }
+    }
+    func Start(){
+        start = Date().timeIntervalSinceReferenceDate
+        isCounting = true
+    }
+    func Stop(){
+        isCounting = false
+    }
     //Sets current time to designated value
-    func Reset(value: Int) -> Bool;
+    func Reset(value: Double = 0.0){
+        currentCount = value;
+    }
 }
